0c94893209a41543af315f3589d696d10d584a27
Oleg Pu <ol74eg@gmail.com>
2016-08-15 11:55:53 +0500

    0.1

---
+++ config.rb
@@ -0,0 +1,20 @@
+
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/"
+http_images_path = "/images"
+http_generated_images_path = "/images"
+http_fonts_path = "/fonts"
+css_path = "D:/dev/Site/esteticclass/project/.cache/compass/stylesheets"
+
+# Local development paths
+sass_path = "D:/dev/Site/esteticclass/project/theme/sass"
+generated_images_path = "D:/dev/Site/esteticclass/project/.cache/compass/images"
+images_path = "D:/dev/Site/esteticclass/project/theme/images"
+fonts_path = "D:/dev/Site/esteticclass/project/theme/fonts"
+
+line_comments = false
+output_style = :compressed
